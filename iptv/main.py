70a44d315cde2d9bf96714a4049bec4ba7011226
back-to <backto@protonmail.ch>
2018-06-24 19:28:12 +0200

    main: basic JSON load test script

--- iptv/main.py
+++ iptv/main.py
@@ -1,6 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import json
+import logging
+import os.path
+
+from fastjsonschema import JsonSchemaException, compile
+from pathlib import Path
+
+from iptv.utils import schema
+
+FORMAT = '[%(name)s][%(levelname)s] %(message)s'
+logging.basicConfig(format=FORMAT)
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
 
 
 def main():
-    print('Hello world.')
+    p = Path(os.path.dirname(__file__))
+    json_files = list(p.glob('data/**/*.json'))
+
+    validate = compile(schema)
+
+    log.info('Load JSON data')
+    data = []
+    for json_file in json_files:
+        with json_file.open() as f:
+            try:
+                data += [validate(json.load(f))]
+            except JsonSchemaException as e:
+                log.error('{0}: {1}'.format(
+                    os.path.join(json_file.parts[-2],
+                                 json_file.parts[-1]),
+                    str(e),
+                ))
+    return log.info('Found {0} channels'.format(len(data)))
