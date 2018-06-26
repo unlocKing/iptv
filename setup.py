80154ba487f0bafde52280185fedb27e4a61dc15
back-to <backto@protonmail.ch>
2018-06-26 14:45:47 +0200

    setup.py: use the same file reader as in pip
    https://github.com/pypa/pip/blob/master/setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import io
+import codecs
 import os
+import re
 
 from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = '\n' + f.read()
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(
+        r'''^__version__ = ['"]([^'"]*)['"]''',
+        version_file,
+        re.M,
+    )
+    if version_match:
+        return version_match.group(1)
+
+    raise RuntimeError('Unable to find version string.')
+
+
+long_description = read('README.md')
 
 setup(
     name='iptv',
-    version='0.0.0',
+    version=find_version('iptv', '__init__.py'),
     description='IPTV database with a M3U playlist generator for LiveProxy.',
     long_description=long_description,
     long_description_content_type='text/markdown',
