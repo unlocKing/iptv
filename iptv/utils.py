3fdfe7fe8627536a3bb1cbc866a46ef5ed523286
back-to <backto@protonmail.ch>
2018-06-27 14:54:08 +0200

    utils: new methods to filter lists
    - comma_list_filter
    - comma_list_filter_remove

--- iptv/utils.py
+++ iptv/utils.py
@@ -5,9 +5,39 @@ log = logging.getLogger(__name__)
 
 
 def comma_list(values):
+    '''
+    converts a comma-separated list into a valid list for python
+    :param values: (string) comma-separated list
+    '''
     return [val.strip() for val in values.split(',')]
+
+
+def comma_list_filter(acceptable):
+    '''
+    creates a function that will only allow strings from 'acceptable'
+    :param acceptable: (list) allowed strings
+    '''
+    def func(p):
+        values = comma_list(p)
+        return list(filter(lambda v: v in acceptable, values))
+
+    return func
+
+
+def comma_list_filter_remove(not_acceptable):
+    '''
+    creates a function that will not allow any strings from 'not_acceptable'
+    :param not_acceptable: (list) not allowed strings
+    '''
+    def func(p):
+        values = comma_list(p)
+        return list(filter(lambda v: v not in not_acceptable, values))
+
+    return func
 
 
 __all__ = [
     'comma_list',
+    'comma_list_filter',
+    'comma_list_filter_remove',
 ]
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,8 @@
 import unittest
 
 from iptv.utils import comma_list
+from iptv.utils import comma_list_filter
+from iptv.utils import comma_list_filter_remove
 
 
 class TestUtils(unittest.TestCase):
@@ -15,8 +17,68 @@ class TestUtils(unittest.TestCase):
                 'data': '/var/run/foo,/var/run/foo',
                 'result': ['/var/run/foo', '/var/run/foo']
             },
+            {
+                'data': 'Das Erste,Россия 24',
+                'result': ['Das Erste', 'Россия 24']
+            },
         ]
 
         for data in test_data:
             self.assertEqual(
                 comma_list(data['data']), data['result'])
+
+    def test_comma_list_filter(self):
+        test_data = [
+            {
+                'acceptable': ['foo', 'bar', 'com'],
+                'data': 'foo,example,bar',
+                'result': ['foo', 'bar']
+            },
+            {
+                'acceptable': ['DE', 'bar', 'com'],
+                'data': 'DE,FR,RU,US',
+                'result': ['DE']
+            },
+            {
+                'acceptable': ['bar', 'com', 'RU'],
+                'data': 'DE,FR,RU,US',
+                'result': ['RU']
+            },
+        ]
+
+        for data in test_data:
+            func = comma_list_filter(data['acceptable'])
+            self.assertEqual(func(data['data']), data['result'])
+
+    def test_comma_list_filter_remove(self):
+        test_data = [
+            {
+                'not_acceptable': ['foo', 'bar', 'com'],
+                'data': 'foo,example,bar',
+                'result': ['example']
+            },
+            {
+                'not_acceptable': ['DE', 'Das Erste', 'bar', 'com'],
+                'data': 'DE,FR,RU,US',
+                'result': ['FR', 'RU', 'US']
+            },
+            {
+                'not_acceptable': ['bar', 'Россия 24', 'foo'],
+                'data': 'Das Erste,Россия 24',
+                'result': ['Das Erste']
+            },
+            {
+                'not_acceptable': ['bar', 'Das Erste', 'foo'],
+                'data': 'Das Erste,Россия 24',
+                'result': ['Россия 24']
+            },
+            {
+                'not_acceptable': ['bar', 'News', 'foo'],
+                'data': 'Das Erste,Россия 24,News',
+                'result': ['Das Erste', 'Россия 24']
+            },
+        ]
+
+        for data in test_data:
+            func = comma_list_filter_remove(data['not_acceptable'])
+            self.assertEqual(func(data['data']), data['result'])
