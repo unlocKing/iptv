148f2afd2ad6bf29a479e2225b42ae6fce612f0d
back-to <backto@protonmail.ch>
2018-07-01 20:01:05 +0200

    temporary SCHEMA change

--- iptv/constants.py
+++ iptv/constants.py
@@ -5,11 +5,11 @@ JSON_SCHEMA = {
     'type': 'object',
     'title': 'IPTV Channel',
     'description': '(Object) Required: Data',
-    'required': ['name', 'streams'],
+    'required': ['streams'],
     'properties': {
         'name': {
              'type': 'string',
-             'description': '(String) Required: A TV Channel Name; eg. TV1'
+             'description': '(String) Optional: A TV Channel Name; eg. TV1'
         },
         'country': {
             'type': 'string',
@@ -49,6 +49,57 @@ JSON_SCHEMA = {
                         'type': 'object',
                         'default': {},
                         'description': '(Object) Optional: additional direct URL options; eg. {"User-Agent": "Mozilla"}'
+                    },
+                    'name': {
+                        'type': 'string',
+                        'description': '(String) Optional and Alternative: A TV Channel Name; eg. TV1'
+                    },
+                    'country': {
+                        'type': 'string',
+                        'description': '(String) Optional and Alternative: An ISO 3166-1 alpha-2 country code; eg. BE',
+                        'minLength': 2,
+                        'maxLength': 2
+                    },
+                    'language': {
+                        'type': 'string',
+                        'description': '(String) Optional and Alternative: An ISO 639-3 language code; eg. eng',
+                        'minLength': 3,
+                        'maxLength': 3
+                    },
+                    'm3u': {
+                        'type': 'object',
+                        'description': '(Object) Optional and Alternative: Metadata for m3u files.',
+                        'properties': {
+                            'logo': {
+                                'type': 'string',
+                                'description': '(String) Optional and Alternative: channel logo URL; eg. http://foo.bar/logo.png'
+                            },
+                            'id': {
+                                'type': 'string',
+                                'description': '(String) Optional and Alternative: TV guide channel ID'
+                            },
+                            'shift': {
+                                'type': 'string',
+                                'description': '(String) Optiona and Alternativel: TV guide time shifting'
+                            },
+                            'name': {
+                                'type': 'string',
+                                'description': '(String) Optional and Alternative: TV guide channel name'
+                            },
+                            'radio': {
+                                'type': 'boolean',
+                                'default': False,
+                                'description': '(Boolean) Optional and Alternative: Audio only; Default=false'
+                            },
+                            'group': {
+                                'type': 'string',
+                                'description': '(String) Optional and Alternative: Group Titles separated by a semicolon; eg. News;Sports'
+                            },
+                            'chno': {
+                                'type': 'string',
+                                'description': '(String) Optional and Alternative: Channel number; eg. 35'
+                            }
+                        }
                     },
                     'attributes': {
                         'type': 'object',
